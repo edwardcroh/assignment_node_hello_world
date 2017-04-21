e44f298d2136edecbaf63e626e332a6581bdea26
Edward <edroh90@gmail.com>
2017-04-20 18:24:53 -0700

    completed assignment

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var chalk = require('chalk');
 var logs = require('./data/logs');
 var logger = require('./lib/logger');
 
-var error = chalk.bold.red;
-
 
 logger.log("Message logged at INFO level", "info");
 logger.log("Message logged at WARNING level", "warning");
